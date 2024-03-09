07ce9e0827f86a88344e1cce99c2547df684d2c9
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-03-09 20:56:24 +0300

    add hateos..

--- licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
@@ -28,6 +31,18 @@ public class LicenseController {
             @PathVariable("licenseId") String licenseId
     ) {
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId()))
+                        .withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
@@ -49,7 +64,7 @@ public class LicenseController {
     }
 
     @DeleteMapping("/{licenseId}")
-    public ResponseEntity<String> deleteEntity(
+    public ResponseEntity<String> deleteLicense(
             @PathVariable("organizationId") String organizationId,
             @PathVariable("licenseId") String licenseId
     ) {
