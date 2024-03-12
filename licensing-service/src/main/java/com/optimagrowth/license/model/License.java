1c98534354ceb93390c9f350b96b2cfc883ac265
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-03-12 21:20:35 +0300

    use jpa

--- licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -1,19 +1,41 @@
 package com.optimagrowth.license.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.hateoas.RepresentationModel;
 
 
 @Getter
 @Setter
 @ToString
-public class License extends RepresentationModel<License> {
-    private int id;
+@Entity
+@Table(name = "licenses")
+public class License {
+
+    @Id
+    @Column(name = "license_Id", nullable = false)
     private String licenseId;
+
     private String description;
+
+    @Column(name = "organization_Id", nullable = false)
     private String organizationId;
+
+    @Column(name = "product_Name", nullable = false)
     private String productName;
+
+    @Column(name = "license_Type", nullable = false)
     private String licenseType;
+
+    @Column(name = "comment")
+    private String comment;
+
+    public License withComment(String comment) {
+        this.setComment(comment);
+        return this;
+    }
 }
